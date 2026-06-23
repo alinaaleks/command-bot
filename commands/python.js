9b9703b69fa5758da0c47177a0732459e6487806
Alina Aleks <toalinaaleks@gmail.com>
2026-06-23 22:03:49 +0300

    fix typos

--- commands/python.js
+++ commands/python.js
@@ -1,7 +1,7 @@
 var keyboard = [
-    [{text: "Imports", callback_data: "/pythonimport"}, {text: "EDA", callback_data: "/pythoneda"}],
+    [{text: "Imports", callback_data: "/pythonimports"}, {text: "EDA", callback_data: "/pythoneda"}],
     [{text: "Missing Values", callback_data: "/pythonmissing"}, {text: "Duplicates", callback_data: "/pythonduplicates"}],
-    [{text: "Type Fixes", callback_data: "/pythontupes"}, {text: "Visuals", callback_data: "/pythonvisuals"}],
+    [{text: "Type Fixes", callback_data: "/pythontypes"}, {text: "Visuals", callback_data: "/pythonvisuals"}],
     [{text: "🔎  Search", callback_data: "/search"}],
     [{text: "HOME", callback_data: "/start"}]
 ];
